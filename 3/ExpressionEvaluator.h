f491aeb91942c4787774c97bcc5ae5c11145c3ea
elizdem <91390634+elizdem@users.noreply.github.com>
2021-12-01 15:48:43 +0300

    Add files via upload

---
+++ 3/ExpressionEvaluator.h
@@ -0,0 +1,27 @@
+﻿#include "ILoggable.h"
+#pragma once
+class ExpressionEvaluator : public ILoggable
+{
+protected:
+	int numOfOperands = 0;
+	double* operands;
+
+	// this thing is used to output arithmetic symbols ¯\_(ツ)_/¯
+	char symbol = '0';
+private:
+	virtual void initializeZeros();
+public:
+	virtual void sort();
+	virtual void swap(size_t i, size_t j);
+	virtual ~ExpressionEvaluator();
+	ExpressionEvaluator(int numOfOperands);
+	ExpressionEvaluator();
+	virtual void setOperand(size_t pos, double value);
+	virtual void setOperands(double ops[], size_t n);
+	virtual double calculate() = 0;
+
+	// implement ILoggable
+	virtual void logToScreen() const override;
+	virtual void logToFile(const std::string& filename) const override;
+};
+
---
+++ 3/Multiplier.h
@@ -0,0 +1,11 @@
+#include "ExpressionEvaluator.h"
+#pragma once
+class Multiplier : public ExpressionEvaluator
+{
+public:
+	Multiplier();
+	Multiplier(int numOfOperands);
+	double calculate() override;
+	bool containZeros();
+};
+
